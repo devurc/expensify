93247d0c9eb9cf80eba9155e83093462ea557170
Eric Horng <erichorng7@gmail.com>
2017-11-22 22:30:04 -0800

    Added react-redux to expense dashboard

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
 import { addExpense } from './actions/expenses';
@@ -14,9 +15,15 @@ store.dispatch(addExpense({ description: 'Water Bill', amount: 23600, createdAt:
 store.dispatch(addExpense({ description: 'Gas Bill', amount: 2600, createdAt: 10500}));
 store.dispatch(addExpense({ description: 'Chinese takeout', amount: 2135, createdAt: 20000}));
 
-// store.dispatch(setTextFilter('water'));
+store.dispatch(setTextFilter('water'));
 
 const state = store.getState();
 console.log(getVisibleExpenses(state.expenses, state.filters));
 
-ReactDOM.render(<AppRouter />, document.getElementById('app'));
+const jsx = (
+  <Provider store={store}>
+    <AppRouter />
+  </Provider>
+);
+
+ReactDOM.render(jsx, document.getElementById('app'));
