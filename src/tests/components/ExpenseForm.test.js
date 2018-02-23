3d685ae8fe2ba535dd14fd1f7509d3d98534da55
Eric Horng <erichorng7@gmail.com>
2018-02-22 18:51:30 -0800

    Finished ExpenseForm user interaction tests

--- src/tests/components/ExpenseForm.test.js
+++ src/tests/components/ExpenseForm.test.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import moment from 'moment';
+import { SingleDatePicker } from 'react-dates';
 import ExpenseForm from '../../components/ExpenseForm';
 import expenses from '../fixtures/expenses';
 
@@ -42,7 +44,6 @@ test('should set note on textarea change', () => {
 });
 
 test('should set amount if valid input', () => {
-//23.50
   const value = '23.50';
   const wrapper = shallow(<ExpenseForm />);
   wrapper.find('input').at(1).simulate('change', {
@@ -52,11 +53,39 @@ test('should set amount if valid input', () => {
 });
 
 test('should not set amount if invalid input', () => {
-// 12.122
   const value = '12.122';
   const wrapper = shallow(<ExpenseForm />);
   wrapper.find('input').at(1).simulate('change', {
     target: { value }
   });
   expect(wrapper.state('amount')).toBe('');
+});
+
+test('should call onSubmit prop for valid form submission', () => {
+  const onSubmitSpy = jest.fn();
+  const wrapper = shallow(<ExpenseForm expense={expenses[0]} onSubmit={onSubmitSpy} />);
+  wrapper.find('form').simulate('submit', { 
+    preventDefault: () => {}
+  });
+  expect(wrapper.state('error')).toBe('');
+  expect(onSubmitSpy).toHaveBeenLastCalledWith({
+    description: expenses[0].description,
+    amount: expenses[0].amount,
+    createdAt: expenses[0].createdAt,
+    note: expenses[0].note
+  });
+});
+
+test('should set new date on date change', () => { 
+  const now = moment();
+  const wrapper = shallow(<ExpenseForm />);
+  wrapper.find(SingleDatePicker).prop('onDateChange')(now);
+  expect(wrapper.state('createdAt')).toBe(now);
+});
+
+test('should set calendar focus on change', () => {
+  const focused = true;
+  const wrapper = shallow(<ExpenseForm />);
+  wrapper.find(SingleDatePicker).prop('onFocusChange')({ focused });
+  expect(wrapper.state('calendarFocused')).toBe(focused);
 });
